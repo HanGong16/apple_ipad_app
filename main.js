9da0baadbe73d94d4d64c0912ed14b157d4e3074
gong16 <hgim96715@gmail.com>
2024-02-13 20:39:38 +0900

    feat: Add search-wrap js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//bascket
 const basketEmotion = document.querySelector('.basket-starter');
 const basketBox = document.querySelector('.basket');
 
@@ -19,4 +20,22 @@ function openBasket() {
 }
 function closeBasket() {
   basketBox.classList.remove('open');
+}
+
+//search-wrap
+const headerEl = document.querySelector('header');
+const searchStarterEl = headerEl.querySelector('.search-starter');
+const searchWrapEl = headerEl.querySelector('.search-wrap');
+const searchCloserEl = searchWrapEl.querySelector(' .search-closer');
+const searchShadowEl = searchWrapEl.querySelector('.shadow');
+
+searchStarterEl.addEventListener('click', showSearch);
+searchCloserEl.addEventListener('click', hideSearch);
+searchShadowEl.addEventListener('click', hideSearch);
+
+function showSearch() {
+  headerEl.classList.add('searching');
+}
+function hideSearch() {
+  headerEl.classList.remove('searching');
 }
