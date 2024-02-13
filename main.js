0bd05726631bd7ccb4435e4c1b12b4e15b3cd261
gong16 <hgim96715@gmail.com>
2024-02-13 21:21:50 +0900

    feat: Add search-wrap animation
    header menu들이 순차적으로 사라지고 순차적으로 나타남을 구현

--- main.js
+++ main.js
@@ -24,10 +24,12 @@ function closeBasket() {
 
 //search-wrap
 const headerEl = document.querySelector('header');
+const headerMenuEls = [...headerEl.querySelectorAll('ul.menu >li')];
 const searchStarterEl = headerEl.querySelector('.search-starter');
 const searchWrapEl = headerEl.querySelector('.search-wrap');
 const searchCloserEl = searchWrapEl.querySelector(' .search-closer');
 const searchShadowEl = searchWrapEl.querySelector('.shadow');
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')];
 
 searchStarterEl.addEventListener('click', showSearch);
 searchCloserEl.addEventListener('click', hideSearch);
@@ -36,8 +38,31 @@ searchShadowEl.addEventListener('click', hideSearch);
 function showSearch() {
   headerEl.classList.add('searching');
   document.documentElement.classList.add('fixed');
+  headerMenuEls
+    .reverse()
+    .forEach(
+      (el, idx) =>
+        (el.style.transitionDelay = (idx * 0.4) / headerMenuEls.length + 's')
+    );
+  searchDelayEls.forEach(
+    (el, idx) =>
+      (el.style.transitionDelay = (idx * 0.4) / searchDelayEls.length + 's')
+  );
 }
 function hideSearch() {
   headerEl.classList.remove('searching');
   document.documentElement.classList.remove('fixed');
+  headerMenuEls
+    .reverse()
+    .forEach(
+      (el, idx) =>
+        (el.style.transitionDelay = (idx * 0.4) / headerMenuEls.length + 's')
+    );
+  searchDelayEls
+    .reverse()
+    .forEach(
+      (el, idx) =>
+        (el.style.transitionDelay = (idx * 0.4) / searchDelayEls.length + 's')
+    );
+  searchDelayEls.reverse();
 }
